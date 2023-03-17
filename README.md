f59c495c098bd30ae3e4efb51760cf3d9165a152
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2023-03-17 13:41:51 -0600

    📄 docs: ReadMe and Formatting

---
+++ README.md
@@ -0,0 +1,29 @@
+# Coolors to Blender Color Ramp
+
+Blender addon to convert a [coolors](https://coolors.co/palettes/trending) url to a color ramp to allow for easy experimentation with colors.
+
+## Installation
+
+1. Download the latest release from the [releases page](https://github.com/Kalmarv/blender-coolors-addon/releases)
+2. In Blender, go to Edit > Preferences > Add-ons > Install
+3. Then select the downloaded zip file and click Install Add-on
+4. The addon should now be installed and you can enable it by checking the box next to it
+
+## Usage
+
+In the Shader Editor, press N to open the sidebar, then click the Coolors tab. Paste a coolors url into the text box and click the "Create Node" button. The color ramp should now be created and you can use it in your shader.
+
+Updating the coolors url and clicking the "Create Node" button will update the color ramp with the new colors. This is the first version of the addon, so the UI and functionality may change in the future.
+
+## Example
+
+Turn this coolors url: `https://coolors.co/palette/f4f1de-e07a5f-3d405b-81b29a-f2cc8f`
+![Example](https://i.imgur.com/bpmqrb4.png)
+
+Into this color ramp with a single click:
+
+![Example](https://i.imgur.com/NXwJiYw.png)
+
+## License
+
+MIT License
--- coolors.py
+++ coolors.py
@@ -1,4 +1,16 @@
+bl_info = {
+    "name": "Coolors to Color Ramp",
+    "description": "Generate a color ramp from a Coolors URL",
+    "author": "Kalmarv",
+    "version": (1, 0),
+    "blender": (3, 0, 0),
+    "location": "Shader Node Editor > Sidebar",
+    "wiki_url": "https://github.com/Kalmarv/blender-coolors-addon",
+    "category": "Material",
+}
+
 import bpy
+
 
 # thank you https://blender.stackexchange.com/a/158902
 def create_cols(_url):
@@ -6,18 +18,18 @@ def create_cols(_url):
         if c < 0:
             return 0
         elif c < 0.04045:
-            return c/12.92
+            return c / 12.92
         else:
-            return ((c+0.055)/1.055)**2.4
+            return ((c + 0.055) / 1.055) ** 2.4
 
     def hex_to_rgb(h, alpha=1):
-        r = (h & 0xff0000) >> 16
-        g = (h & 0x00ff00) >> 8
-        b = (h & 0x0000ff)
-        return tuple([srgb_to_linearrgb(c/0xff) for c in (r, g, b)] + [alpha])
+        r = (h & 0xFF0000) >> 16
+        g = (h & 0x00FF00) >> 8
+        b = h & 0x0000FF
+        return tuple([srgb_to_linearrgb(c / 0xFF) for c in (r, g, b)] + [alpha])
 
     slash_index = _url.rfind("/")
-    pallate_str = _url[slash_index + 1:]
+    pallate_str = _url[slash_index + 1 :]
     arr = pallate_str.split("-")
     rgba_arr = [hex_to_rgb(int(c, 16)) for c in arr]
     return rgba_arr
@@ -26,8 +38,8 @@ def create_cols(_url):
 class CoolorsPanel(bpy.types.Panel):
     bl_label = "Coolors Panel"
     bl_idname = "OBJECT_PT_coolors_panel"
-    bl_space_type = 'NODE_EDITOR'
-    bl_region_type = 'UI'
+    bl_space_type = "NODE_EDITOR"
+    bl_region_type = "UI"
     bl_category = "Coolors"
 
     def draw(self, context):
@@ -37,14 +49,14 @@ class CoolorsPanel(bpy.types.Panel):
         row = layout.row()
         row.operator("my_addon.print_text")
 
+
 class GetCoolorsOperator(bpy.types.Operator):
     bl_idname = "my_addon.print_text"
     bl_label = "Create Node"
 
     def execute(self, context):
-
         active_material = bpy.context.object.active_material
-        
+
         if active_material is not None:
             # Color data
             cols_arr = create_cols(context.scene.coolors_url)
@@ -52,13 +64,17 @@ class GetCoolorsOperator(bpy.types.Operator):
 
             # Selecting node if it already exists, otherwise create a new one
             if active_material.node_tree.nodes.get("generated_coolors") is not None:
-                color_ramp_node = active_material.node_tree.nodes.get("generated_coolors")
+                color_ramp_node = active_material.node_tree.nodes.get(
+                    "generated_coolors"
+                )
             else:
-                color_ramp_node = active_material.node_tree.nodes.new(type='ShaderNodeValToRGB')
+                color_ramp_node = active_material.node_tree.nodes.new(
+                    type="ShaderNodeValToRGB"
+                )
                 # Color Ramp properties
                 color_ramp_node.name = "generated_coolors"
                 color_ramp_node.label = "Coolors Palette"
-            
+
             # Remove all colors but one, Blender will not allow you to delete them all
             for i in range(len(color_ramp_node.color_ramp.elements) - 1):
                 element = color_ramp_node.color_ramp.elements[0]
@@ -68,10 +84,14 @@ class GetCoolorsOperator(bpy.types.Operator):
             # we left over in the previous step
             for i in range(cols_len):
                 if i != cols_len - 1:
-                    element = color_ramp_node.color_ramp.elements.new((i / (cols_len - 1)))
+                    element = color_ramp_node.color_ramp.elements.new(
+                        (i / (cols_len - 1))
+                    )
                     element.color = cols_arr[i]
                 else:
-                    element = color_ramp_node.color_ramp.elements[len(color_ramp_node.color_ramp.elements)-1]
+                    element = color_ramp_node.color_ramp.elements[
+                        len(color_ramp_node.color_ramp.elements) - 1
+                    ]
                     element.color = cols_arr[i]
                     # In case the elments were moved
                     element.position = 1.0
@@ -79,26 +99,27 @@ class GetCoolorsOperator(bpy.types.Operator):
             # Create and select node
             active_material.node_tree.nodes.active = color_ramp_node
             color_ramp_node.select = True
-            #active_material.node_tree.nodes.update()
 
-            return {'FINISHED'}
+            return {"FINISHED"}
 
         else:
-            self.report({'ERROR'}, "No active material")
-            return {'FINISHED'}
+            self.report({"ERROR"}, "No active material")
+            return {"FINISHED"}
+
 
 def register():
     bpy.types.Scene.coolors_url = bpy.props.StringProperty(
-        name="URL",
-        description="Enter Coolors palette url"
+        name="URL", description="Enter Coolors palette url"
     )
     bpy.utils.register_class(CoolorsPanel)
     bpy.utils.register_class(GetCoolorsOperator)
+
 
 def unregister():
     del bpy.types.Scene.coolors_url
     bpy.utils.unregister_class(CoolorsPanel)
     bpy.utils.unregister_class(GetCoolorsOperator)
+
 
 if __name__ == "__main__":
     register()
